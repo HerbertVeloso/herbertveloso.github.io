67003a23d288f4179606edc8b4cbb09525ef172c
Vai na Web <vainawebcpx@gmail.com>
2018-03-07 13:47:26 -0300

    Add responsive

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Navegação da página
 $('.scroll').click(function(event){
     event.preventDefault();
     var id = $(this).attr('href'),
@@ -6,4 +7,18 @@ $('.scroll').click(function(event){
     $('html, body').animate({
         scrollTop: targetOffset - menuHeight
     }, 500);
+});
+
+// Menu Mobile
+$(".navbar-links").addClass("closed");
+$('.navbar-bars').click(function(){
+    if($(".navbar-links").hasClass("closed")) {
+        $(".navbar-links").animate({top:'8vh'},{duration:400}).removeClass("closed");
+    }
+    else {
+        $(".navbar-links").animate({top:'-18vh'},{duration:400}).addClass("closed");
+    }
+});
+$('.navbar-link').click(function(){
+        $(".navbar-links").animate({top:'-18vh'},{duration:400}).addClass("closed");
 });
